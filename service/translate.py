75043e1f8f5e778e49388f6733a4db9e95b7cd3f
Vridhi Lalwani <vl4334@srmist.edu.in>
2025-07-21 00:35:40 +0530

    Added translate utility

--- service/translate.py
+++ service/translate.py
@@ -1,8 +1,43 @@
-from fastapi import FastAPI
-from models.translateModel import TranslationResponse
 
+from models.translateModel import TranslationResponse,TranslationRequest
+import os
+from fastapi import FastAPI, HTTPException, Query
+# Import GenerativeModel instead of ChatModel from preview
+from vertexai.generative_models import GenerativeModel, Part, GenerationConfig # Import necessary classes
+from google.cloud import aiplatform
+
+
+# Set credentials if running locally
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "vertexgen-466509-702ac76e7f0a.json"
+
+# FastAPI app
 app = FastAPI()
 
-@app.get("/translate", response_model=TranslationResponse)
-def get_translation():
-    return TranslationResponse()
+@app.post("/translate", response_model=TranslationResponse)
+def get_translation( request: TranslationRequest):
+    try:
+        # Initialize the Gemini model
+        model = GenerativeModel("gemini-2.0-flash")
+
+        # Start chat
+        chat_session = model.start_chat()
+
+        # Prepare prompt
+        prompt = f"Translate the following sentence to {request.target_language}. Respond ONLY with the translated text. Do NOT include any explanation, transliteration, or alternative options:\n\n{request.text}"
+
+        # Send prompt
+        response = chat_session.send_message(prompt)
+
+        # Extract trasnslation
+        translated = response.candidates[0].content.parts[0].text.strip()
+        print(translated)
+        return TranslationResponse(translated_phrase=translated)
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+# For local run
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("service.translate:app", host="127.0.0.1", port=8000, reload=True)
